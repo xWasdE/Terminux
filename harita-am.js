9c0ab7e3a3747b55b819ed1ca8ad380a514be745
xWasdE <79269306+xWasdE@users.noreply.github.com>
2026-08-16 10:35:44 +0300

    Update harita-am.js

--- harita-am.js
+++ harita-am.js
@@ -1,16 +1,16 @@
 window.haritaAmeliyathane = `
 <svg id="path-svg-am" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; pointer-events: none; z-index: 9999; overflow: visible;"></svg>
 
-<div id="door-am" style="position: absolute; top: 0.2%; left: 2.3%; width: 9.9%; height: 11.9%; display: flex; align-items: center; justify-content: center; color: #888; font-weight: 900; font-size: 16px; letter-spacing: 2px; border-right: 2px dashed #666; border-bottom: 2px dashed #666; z-index: 5;">KAPI <span style="position: absolute; right: 10px; top: 50%; transform: translateY(-50%); font-size: 20px; color: #aaa;">↓</span></div>
+<div id="door-am" style="position: absolute; top: 0.2%; left: 5.8%; width: 12.7%; height: 10.8%; display: flex; align-items: center; justify-content: center; color: #888; font-weight: 900; font-size: 16px; letter-spacing: 2px; border-right: 2px dashed #666; border-bottom: 2px dashed #666; z-index: 5;">KAPI <span style="position: absolute; right: 10px; top: 50%; transform: translateY(-50%); font-size: 20px; color: #aaa;">↓</span></div>
 
-<div style="position: absolute; top: 0.0%; left: 90.2%; width: 8.9%; height: 25.8%; border: 2px solid #333; background: #0a0a0a; display: flex; flex-direction: column; align-items: center; justify-content: center; color: #666; border-radius: 8px; text-align: center; z-index: 5;"><div style="font-weight: 900; font-size: 14px; margin-bottom: 2px; letter-spacing: 2px; transform: rotate(-90deg);">MASA</div></div>
+<div style="position: absolute; top: -0.2%; left: 86.7%; width: 12.3%; height: 24.9%; border: 2px solid #333; background: #0a0a0a; display: flex; flex-direction: column; align-items: center; justify-content: center; color: #666; border-radius: 8px; text-align: center; z-index: 5;"><div style="font-weight: 900; font-size: 14px; margin-bottom: 2px; letter-spacing: 2px; transform: rotate(-90deg);">MASA</div></div>
 
-<div style="position: absolute; top: 12.3%; left: 0.3%; width: 10.7%; height: 19.4%; display: grid; grid-template-columns: 1fr; grid-template-rows: 1fr 1fr; gap: 8px; z-index: 5;">
+<div style="position: absolute; top: 11.5%; left: 0.1%; width: 14.1%; height: 20.8%; display: grid; grid-template-columns: 1fr; grid-template-rows: 1fr 1fr; gap: 8px; z-index: 5;">
     <div id="cell-05.01.01" class="cell cell-lens">05.01.01</div>
     <div id="cell-04.01.01" class="cell cell-lens">04.01.01</div>
 </div>
 
-<div class="map-shelf" style="position: absolute; top: 31.6%; left: 0.1%; width: 44.6%; height: 67.0%; display: grid; grid-template-columns: repeat(5, 1fr); grid-template-rows: repeat(3, 1fr); gap: 2px; background: #1a1a1a; padding: 4px; border: 2px solid #333; border-radius: 4px; z-index: 5;">
+<div class="map-shelf" style="position: absolute; top: 33.2%; left: 0.3%; width: 43.9%; height: 65.0%; display: grid; grid-template-columns: repeat(5, 1fr); grid-template-rows: repeat(3, 1fr); gap: 2px; background: #1a1a1a; padding: 4px; border: 2px solid #333; border-radius: 4px; z-index: 5;">
     <div id="cell-07.01.05" class="cell">07.01.05</div>
     <div id="cell-07.01.04" class="cell">07.01.04</div>
     <div id="cell-07.01.03" class="cell">07.01.03</div>
@@ -28,7 +28,7 @@ window.haritaAmeliyathane = `
     <div id="cell-01.02.01" class="cell">01.02.01</div>
 </div>
 
-<div class="map-shelf" style="position: absolute; top: 25.9%; left: 54.5%; width: 44.6%; height: 72.8%; display: grid; grid-template-columns: repeat(6, 1fr); grid-template-rows: repeat(3, 1fr); gap: 2px; background: #1a1a1a; padding: 4px; border: 2px solid #333; border-radius: 4px; z-index: 5;">
+<div class="map-shelf" style="position: absolute; top: 25.6%; left: 51.2%; width: 47.9%; height: 73.2%; display: grid; grid-template-columns: repeat(6, 1fr); grid-template-rows: repeat(3, 1fr); gap: 2px; background: #1a1a1a; padding: 4px; border: 2px solid #333; border-radius: 4px; z-index: 5;">
     <div id="cell-02.01.01" class="cell">02.01.01</div>
     <div id="cell-02.01.02" class="cell">02.01.02</div>
     <div id="cell-02.01.03" class="cell">02.01.03</div>
